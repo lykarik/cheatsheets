bf2d49fbcbac80c7473f49bea2f061852819561c
Andrew <9yards.to.nowhere@gmail.com>
2022-06-18 01:39:55 +0500

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,4 @@
+#A solution would be to force the key files to be kept permanently, by adding them in your ~/.ssh/config file:
+
+IdentityFile ~/.ssh/gitHubKey
+IdentityFile ~/.ssh/id_rsa_buhlServer

b1095bc6ba743fe99d1bebab9c50166b3abbc4a1
Andrew <9yards.to.nowhere@gmail.com>
2022-08-19 16:51:51 +0500

    Create jenkins_blank.sh

---
+++ install _blanks/jenkins_blank.sh
@@ -0,0 +1,8 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
